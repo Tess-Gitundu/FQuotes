d5367870004e77eba5b4440819cea4f9e3dc423a
Tess <njerigitundu003@gmail.com>
2022-04-09 17:41:57 +0300

    Add a form structure

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
+  
+  newQuote = new Quote (0, "", "", "", 0, 0, new Date());
   constructor() { }
 
   ngOnInit(): void {
